0f3533dad4517613b64c77b1a2bfe00a0e97dd1b
Nacra67 <nacra67@gmail.com>
2023-10-16 09:13:09 +0200

    Revert "Internacionalización main"
    This reverts commit e2b7153908ebbe6d2cd39c37213573e54ad61954, reversing
    changes made to dc0ed748ca8b06cf392074ec9c122c5ddbe99de0.

--- camerafront/src/app/shared/app.menu.config.ts
+++ camerafront/src/app/shared/app.menu.config.ts
@@ -2,6 +2,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'products', name: 'PRODUCTS', icon: 'video_camera_front', route: '/main/products' },
+  { id: 'products', name: 'PRODUCTS', icon: 'box', route: '/main/products' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
