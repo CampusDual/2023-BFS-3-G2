60b15c77ed1dd4af36b51b64fbae4307eac80ef5
Ruli <raulmendozacristobal@gmail.com>
2023-10-16 09:30:28 +0200

    Css

--- camerafront/src/app/shared/app.menu.config.ts
+++ camerafront/src/app/shared/app.menu.config.ts
@@ -1,7 +1,18 @@
-import { MenuRootItem } from 'ontimize-web-ngx';
+import { MenuRootItem } from "ontimize-web-ngx";
 
 export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'products', name: 'PRODUCTS', icon: 'box', route: '/main/products' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+  { id: "home", name: "HOME", icon: "home", route: "/main/home" },
+  {
+    id: "products",
+    name: "PRODUCTS",
+    icon: "video_camera_front",
+    route: "/main/products",
+  },
+  {
+    id: "logout",
+    name: "LOGOUT",
+    route: "/login",
+    icon: "power_settings_new",
+    confirm: "yes",
+  },
 ];
