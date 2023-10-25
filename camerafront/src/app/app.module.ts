245bb62497ed5cc5e6844289cc6a566b68a4d5cf
Ruli <raulmendozacristobal@gmail.com>
2023-10-25 10:13:57 +0200

    dark mode

--- camerafront/src/app/app.module.ts
+++ camerafront/src/app/app.module.ts
@@ -1,34 +1,41 @@
-import { NgModule } from '@angular/core';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { APP_CONFIG, ONTIMIZE_MODULES, ONTIMIZE_PROVIDERS, OntimizeWebModule } from 'ontimize-web-ngx';
+import { NgModule } from "@angular/core";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import {
+  APP_CONFIG,
+  ONTIMIZE_MODULES,
+  ONTIMIZE_PROVIDERS,
+  O_MAT_ERROR_OPTIONS,
+  OntimizeWebModule,
+} from "ontimize-web-ngx";
 
-import { environment } from '../environments/environment';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CONFIG } from './app.config';
+import { environment } from "../environments/environment";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CONFIG } from "./app.config";
 
 // Standard providers...
 // Defining custom providers (if needed)...
-export const customProviders: any = [
-];
+export const customProviders: any = [];
 
 @NgModule({
   imports: [
     ONTIMIZE_MODULES,
     OntimizeWebModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
   ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   providers: [
     { provide: APP_CONFIG, useValue: CONFIG },
     ONTIMIZE_PROVIDERS,
-    ...customProviders
+    {
+      provide: O_MAT_ERROR_OPTIONS,
+      useValue: { type: "lite" },
+      ...customProviders,
+    },
   ],
 })
-export class AppModule { }
+export class AppModule {}
