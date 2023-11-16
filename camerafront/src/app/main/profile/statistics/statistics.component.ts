e082f930d6b78153e00ae5952613517a3c868608
Nacra67 <nacra67@gmail.com>
2023-11-16 12:02:43 +0100

    pichart

--- camerafront/src/app/main/profile/statistics/statistics.component.ts
+++ camerafront/src/app/main/profile/statistics/statistics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Expression, FilterExpressionUtils, OFormComponent } from 'ontimize-web-ngx';
+import { Expression, FilterExpressionUtils, OFormComponent, OTableComponent } from 'ontimize-web-ngx';
 import { OChartComponent, } from 'ontimize-web-ngx-charts';
 import { } from 'src/app/shared/d3-locale/d3-locale.service';
 
@@ -9,17 +9,17 @@ import { } from 'src/app/shared/d3-locale/d3-locale.service';
   styleUrls: ['./statistics.component.css']
 })
 
-export class StatisticsComponent{
+export class StatisticsComponent {
   public selected: {};
   @ViewChild('pie', { static: true }) pie: OChartComponent;
   @ViewChild('formFilter', { static: false }) private formFilter: OFormComponent;
-
+  title: string;
+  profits: any;
   constructor() {
-
   }
 
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.filterBuilder()
   }
 
@@ -28,6 +28,7 @@ export class StatisticsComponent{
     if (dates) {
       const startDate = dates.startDate.format('YYYY-MM-DD')
       const endDate = dates.endDate.format('YYYY-MM-DD')
+      this.title = "PERIODO " + startDate + " - " + endDate;
       // console.log(startDate)
       // const filterExpr = FilterExpressionUtils.buildExpressionLess("start_date", fecha);
       // const basicExpr = FilterExpressionUtils.buildBasicExpression(filterExpr);
@@ -38,14 +39,21 @@ export class StatisticsComponent{
         '@basic_expression': filters.reduce((exp1, exp2) =>
           FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND))
       }
-      kv["state"]= "applied"
+      kv["state"] = "applied"
       this.pie.queryData(kv, { sqltypes: { start_date: 91 } });
     }
     else {
+      this.title = "ALL TIME"
       const filterExpr = FilterExpressionUtils.buildExpressionLike("state", "applied");
       const basicExpr = FilterExpressionUtils.buildBasicExpression(filterExpr);
       this.pie.queryData(basicExpr);
-    }
 
+
+    }
+    
+  }
+  loadData(){
+    let dataProfits = this.pie.getDataArray()
+    console.log(dataProfits)
   }
 }
